c1ed308d86348fb1d4df23c0db36b7a084261427
Nathan <nathan@nathanmccusker.com>
2018-05-19 13:22:20 -0700

    Adding contact us form

--- views.py
+++ views.py
@@ -3,3 +3,7 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, 'website/home.html')
+
+
+def contact(request):
+    return render(request, 'website/contact.html')
