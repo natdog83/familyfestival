f891cb155746dce3af0ba613333c0db225c7644d
Nathan <nathan@nathanmccusker.com>
2018-05-20 00:11:44 -0700

    Updating urls

--- urls.py
+++ urls.py
@@ -4,6 +4,6 @@ from . import views
 
 app_name = 'website'
 urlpatterns = [
-    url('^', views.index, name='home'),
-    url(r'^contact/$', views.contact, name='contact'),
+    path('^', views.index, name='home'),
+    path(r'^contact/$', views.contact, name='contact'),
 ]
