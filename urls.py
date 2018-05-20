435649294e07ad518a8ea3135da35ee8cfad949f
Nathan <nathan@nathanmccusker.com>
2018-05-20 00:34:28 -0700

    Urls

--- urls.py
+++ urls.py
@@ -2,8 +2,7 @@ from django.conf.urls import url
 from django.urls import path
 from . import views
 
-app_name = 'website'
 urlpatterns = [
-    url('^', views.index, name='home'),
+    url('^$', views.index, name='home'),
     url(r'^contact/$', views.contact, name='contact'),
 ]
