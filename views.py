785dcb32c16ab6f96534dda11d743fc44e3a97b3
Nathan <nathan@nathanmccusker.com>
2018-05-19 22:26:30 -0700

    fixing the views

--- views.py
+++ views.py
@@ -11,39 +11,4 @@ def index(request):
 
 
 def contact(request):
-    form_class = ContactForm
-
-    # new logic!
-    if request.method == 'POST':
-        form = form_class(data=request.POST)
-
-        if form.is_valid():
-            contact_name = request.POST.get(
-                'name', '')
-            contact_email = request.POST.get(
-                'email', '')
-            form_content = request.POST.get('message', '')
-
-            # Email the profile with the
-            # contact information
-            template = get_template('contact_template.txt')
-            context = {
-                'name': name,
-                'email': email,
-                'message': message,
-            }
-            content = template.render(context)
-
-            email = EmailMessage(
-                "New contact form submission",
-                content,
-                "Your website" + '',
-                ['youremail@gmail.com'],
-                headers={'Reply-To': email}
-            )
-            email.send()
-            return redirect('contact')
-
-    return render(request, 'website/contact.html', {
-        'form': form_class,
-    })
+    return render(request, 'website/contact.html')
